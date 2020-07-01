520447c91c3612377a922c8443cfddc04a6d77ba
Luan Portugal <luanportugal.contato@gmail.com>
2020-06-30 22:47:05 -0300

    Exclused auth paths from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,6 +2,12 @@ const { verifyJwt } = require('../helpers/jwt')
 
 const checkJwt = (req, res, next) => {
 
+    const { url: path } = req
+
+    const exclusedPaths = ['/auth/sign-in', '/auth/sign-up']
+    const isExclused = !!exclusedPaths.find(p => p.startsWith(path))
+    if (isExclused) return next()
+
 
     let token = req.headers['authorization']
     token = token ? token.slice(7, token.length) : null
